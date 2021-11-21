1ec34f18c4064f46116d09bac7f1f0b69e5c11fc
PerciusT <shari200089@gmail.com>
2021-11-21 21:19:07 +0500

    Add files via upload

--- assets/js/register.js
+++ assets/js/register.js
@@ -19,4 +19,42 @@ $(document).ready(function()
         $("#home-tab").removeClass("active");
         
     });
+    $("#passdrivec").keyup(passcheckd);
+    $("#passridec").keyup(passcheckr);
+    $("#passride").keyup(passcheckr);
+    $("#passdrive").keyup(passcheckd);
+    function passcheckd()
+    { 
+        
+        if($("#passdrive").val() == $("#passdrivec").val())
+        {
+            $("#passdrive").removeClass("wrongpass");
+            $("#passdrivec").removeClass("wrongpass");
+            $("#btnRegisterID1").prop('disabled',false);
+        }
+        else
+        {
+            $("#passdrivec").addClass("wrongpass");
+            $("#passdrive").addClass("wrongpass");
+            $("#btnRegisterID1").prop('disabled',true);
+        }
+
+    }
+    function passcheckr()
+    {
+        if($("#passride").val() == $("#passridec").val())
+        {
+            $("#passride").removeClass("wrongpass");
+            $("#passridec").removeClass("wrongpass");
+            $("#btnRegisterID").prop('disabled',false);
+            
+        }
+        else
+        {
+            $("passride").addClass("wrongpass");
+            $("#passridec").addClass("wrongpass");
+            $("#btnRegisterID").prop('disabled',true);   
+        }
+    }
+    
 });
