6d0ec6ff35a1a7ce51f1c42e7cbf39a4683c0b84
PerciusT <shari200089@gmail.com>
2021-11-30 13:28:10 +0500

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAoFg6Hf6UrrwTT4cmOOahzOLdiTYq3h4Y",
+    authDomain: "garikar-39b0f.firebaseapp.com",
+    projectId: "garikar-39b0f",
+    storageBucket: "garikar-39b0f.appspot.com",
+    messagingSenderId: "515003222323",
+    appId: "1:515003222323:web:ffed3bbf2b716432bd1646"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+async function getdata(db) {
+    
+  }
